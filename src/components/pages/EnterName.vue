9b65c05491a1307b242a82ac46c6060b7d82c5e7
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-01-17 16:37:54 -0500

    Added conditional routing to check if a user has entered their name

---
+++ src/components/pages/EnterName.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Enter Your Name</h1>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'EnterName'
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import EnterName from '@/components/pages/EnterName'
 import HomePage from '@/components/pages/HomePage'
 import EditDay from '@/components/pages/EditDay'
 
-const routes = [
-  { path: '/', component: HomePage, name: 'HomePage' },
-  { path: '/edit/:day', component: EditDay, name: 'EditDay' }
-]
+let routes = []
+
+if (window.localStorage.getItem('user')) {
+  routes = [
+    { path: '/', component: HomePage, name: 'HomePage' },
+    { path: '/edit/:day', component: EditDay, name: 'EditDay' }
+  ]
+} else {
+  routes = [{ path: '/:catchAll(.*)', component: EnterName, name: 'EnterName' }]
+}
 
 const router = createRouter({
   history: createWebHistory(),
