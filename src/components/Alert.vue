b0024fe93a807486cb140a4b18ce74cb9c4e9dd2
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-05-22 22:19:27 -0400

    Created component

---
+++ src/components/Alert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: ['msg']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/global.scss';
+
+</style>
