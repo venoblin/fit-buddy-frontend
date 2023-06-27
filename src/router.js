e4ae05e9d493d8ff2f1ea67060755e19b089c0e6
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-26 21:53:26 -0400

    Added profile route

--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EnterName from '@/components/pages/EnterName'
 import HomePage from '@/components/pages/HomePage'
 import EditDay from '@/components/pages/EditDay'
+import UserProfile from '@/components/pages/UserProfile'
 
 let routes = []
 
 if (window.localStorage.getItem('user')) {
   routes = [
     { path: '/', component: HomePage, name: 'HomePage' },
-    { path: '/edit/:day', component: EditDay, name: 'EditDay' }
+    { path: '/edit/:day', component: EditDay, name: 'EditDay' },
+    { path: '/profile', component: UserProfile, name: 'UserProfile' }
   ]
 } else {
   routes = [{ path: '/:catchAll(.*)', component: EnterName, name: 'EnterName' }]
