a1d7e94b2b1a76176606d36409854d96bb98ea22
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-27 23:52:39 -0400

    Added edit name button

--- src/components/pages/UserProfile.vue
+++ src/components/pages/UserProfile.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>{{ `${user}'s` }} Profile</h1>
-  <button class="danger" @click="deleteUser">Delete User</button>
+
+  <div class="links">
+    <button @click="toggleInput">Edit Name</button>
+    <button class="danger" @click="deleteUser">Delete User</button>
+  </div> 
 </template>
 
 <script>
@@ -9,12 +13,16 @@ import { useUserStore } from '@/stores/user'
 export default {
   name: 'UserProfile',
   data: () => ({
-    user: useUserStore().user
+    user: useUserStore().user,
+    isEditing: false
   }),
   methods: {
     deleteUser() {
       useUserStore().deleteUser()
       this.$router.push('/')
+    },
+    toggleInput() {
+      this.isEditing = true
     }
   }
 }
