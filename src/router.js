9275ee3e765d7437ca0aa0971882d8dcae1e71a7
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-01 01:35:31 -0400

    Added not found to routes

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EnterName from '@/components/pages/EnterName'
+import NotFound from '@/components/pages/NotFound'
 import HomePage from '@/components/pages/HomePage'
 import EditDay from '@/components/pages/EditDay'
 import UserProfile from '@/components/pages/UserProfile'
@@ -8,7 +8,7 @@ let routes = [
   { path: '/', component: HomePage, name: 'HomePage' },
   { path: '/edit/:day', component: EditDay, name: 'EditDay' },
   { path: '/profile', component: UserProfile, name: 'UserProfile' },
-  { path: '/:catchAll(.*)', component: EnterName, name: 'EnterName' }
+  { path: '/:catchAll(.*)', component: NotFound, name: 'NotFound' }
 ]
 
 const router = createRouter({
