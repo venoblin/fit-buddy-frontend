dd02b3c582712777a08a6f68f5f594b3f9120da4
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-08-17 16:16:13 -0400

    added loading styles

--- src/components/LoadingPopUp.vue
+++ src/components/LoadingPopUp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>LOADING</div>
+  <div class="loader"></div>
 </template>
 
 <script>
@@ -8,4 +8,27 @@
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/styles/global.scss';
+
+.loader {
+    width: 48px;
+    height: 48px;
+    margin: 3rem;
+    border: 5px solid $green;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+
+    @keyframes rotation {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    } 
+}
+</style>
